89e6b76d4d4648131635776a33ff87f4acbf3d8e
Vipin <dev.wonderly@f1studioz.com>
2024-10-07 12:26:46 +0530

    module scss replaced

--- src/components/common/Summary.tsx
+++ src/components/common/Summary.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { FaChevronRight } from "react-icons/fa6";
-import styles from "./Summary.module.scss";
+import "./Summary.scss";
 import Image from "next/image";
 import { Card } from "react-bootstrap";
 
 const SummarySection = ({ summaryData }: { summaryData: {label:string,value:string,image:string}[] }) => {
   return (
-    <section className={styles.summary}>
+    <section className={"summary"}>
       {
         summaryData.map(summary=>{
           return (
-            <Card className={`internal-card ${styles.summaryItem}`} key={summary.label}>
-            <div className={styles.iconContainer}>
+            <Card className={`internal-card summaryItem`} key={summary.label}>
+            <div className={"iconContainer"}>
               <Image src={summary.image} width={summary.image==="/employers.png"?20:24} height={24} alt="job" />
             </div>
-            <div className={styles.textContainer}>
-              <div className={styles.value}>{summary.value}</div>
-              <div className={styles.label}>{summary.label}</div>
+            <div className={"textContainer"}>
+              <div className={"value"}>{summary.value}</div>
+              <div className={"label"}>{summary.label}</div>
             </div>
-            <div className={styles.more}>
+            <div className={"more"}>
              <FaChevronRight fontSize={16} />
     
             </div>
