19f3038af1e84efbb3a4f99f36df0a51efbc4d6c
unknown <dev.wonderly@f1studioz.com>
2024-10-09 16:50:37 +0530

    WON-34 created users screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import Dashboard from '@/components/dashboard/Dashboard';
+import Users from '@/components/users/Users';
+
 import { ReactQueryProvider } from './react-quuery-provider';
 
 const DashboardPage = () => {
