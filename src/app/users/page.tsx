4463e236aa7b9ef453a5d6c1e04d07610ba5ba94
Alekhya Thumati <alekhyathumati7470@gmail.com>
2024-10-10 19:31:56 +0530

    files modified

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,15 +1,38 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import RegisteredUsers from '@/components/dashboard/RegisterdUsers';
+import CreateUserForm from '@/components/Users/CreateUsers';
 import styles from "../../components/common/table/DataTable.module.scss";
+import { Button, Modal } from 'react-bootstrap';
+import UsersRegistered from '@/components/Users/UsersRegisterd';
 
 const Page: React.FC = () => {
+  const [showCreateUser, setShowCreateUser] = useState(false);
+
+  const handleCreateUserClick = () => {
+    setShowCreateUser(true);
+  };
+
+  const handleCancelCreateUser = () => {
+    setShowCreateUser(false);
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>Registered Users</h1>
       <div className={styles.registeredUsers}>
         <RegisteredUsers />
       </div>
+      <Button onClick={handleCreateUserClick} className="btn-img">
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none"></svg> Create User +
+      </Button>
+
+      {/* Modal for Create User Form */}
+      <Modal show={showCreateUser} onHide={handleCancelCreateUser} backdrop={false}>
+        <Modal.Body>
+          <CreateUserForm onCancel={handleCancelCreateUser} />
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
