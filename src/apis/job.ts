3d7db23c810f6d4eae606e1501ce4ea7198728f8
Vipin <dev.wonderly@f1studioz.com>
2024-10-11 13:53:04 +0530

    route changed for job details

--- src/apis/job.ts
+++ src/apis/job.ts
@@ -55,7 +55,7 @@ export const getJobs = async (
 
 export const getJobDetails = async (id: string) => {
   try {
-    const response = await apiClient.get(`${basePath}/${id}`);
+    const response = await apiClient.get(`${basePath}/admin/${id}`);
     console.log("API Response:", response.data);
     return response.data; 
   } catch (error) {
