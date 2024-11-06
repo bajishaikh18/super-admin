0f4055328c08475b1dfb296c9703b2b32687a7a6
Vipin <dev.wonderly@f1studioz.com>
2024-11-06 10:31:10 +0530

    WON-63 notification changes

--- src/components/common/notifications/Notifications.tsx
+++ src/components/common/notifications/Notifications.tsx
@@ -9,6 +9,7 @@ import { useCallback } from 'react';
 import toast from 'react-hot-toast';
 import Link from 'next/link';
 import { FaPlus } from 'react-icons/fa6';
+import { useRouter } from 'nextjs-toploader/app';
 
 export const Notifications = ()=>{
     const {
@@ -16,25 +17,41 @@ export const Notifications = ()=>{
       isLoading,
       error,
     } = useQuery<Notification[]>({ queryKey: ["user-notifications"], queryFn: getUserNotifications, retry:1 });
-    if(isLoading){
-     return <Loader text='Fetching notifications'/>
-    } 
-    if(error){
-     return <NotFound text='Error while fetching notifications'/>
-    }
-    if(!data){
-     return <NotFound text='No notifications'/>
-    }
+    const router = useRouter();
+    const notificationsNew = data?.filter((notif)=>!notif.dismissed);
+    const notificationsPrev = data?.filter((notif)=>notif.dismissed);
 
-    const notificationsNew = data.filter((notif)=>!notif.dismissed);
-    const notificationsPrev = data.filter((notif)=>notif.dismissed);
     const markAsRead = useCallback(async (id:string)=>{
       try{
         await updateNotification(id,{dismissed:true});
+        
       }catch{
         toast.error("Something went wrong while marking notification status");
       }
     },[notificationsNew])
+
+    const openNotification = useCallback(async (notification:Notification)=>{
+      try{
+        await markAsRead(notification._id);
+      }catch(e){
+
+      }
+      if(notification.data){
+        router.push(notification.data);
+      }
+    },[markAsRead])
+
+   
+    if(isLoading){
+      return <Loader text='Fetching notifications'/>
+     } 
+     if(error){
+      return <NotFound text='Error while fetching notifications'/>
+     }
+     if(!data){
+      return <NotFound text='No notifications'/>
+     }
+ 
     return (
         <div className={styles.notificationPanel}>
         <div className={styles.notificationHeader}>
@@ -46,12 +63,12 @@ export const Notifications = ()=>{
         <div className={styles.notificationContentContainer}>
         <div className={styles.notificationSection}>
             {
-                notificationsNew.length > 0  && <div className={styles.notificationCreateHeader}> <h4>New ({notificationsNew.length})</h4> <Link href='/notifications'><FaPlus fontSize={12}/> Create custom</Link></div>
+                 <div className={styles.notificationCreateHeader}>{notificationsNew && notificationsNew?.length > 0  && <h4>New ({notificationsNew.length})</h4>}<Link href='/notifications'><FaPlus fontSize={12}/> Create custom</Link></div>
             }
             {
-                notificationsNew.map(notification=>{
+                notificationsNew?.map(notification=>{
                     return (
-                        <div className={styles.notificationItem} onClick={()=>markAsRead(notification._id)}>
+                        <div className={styles.notificationItem} onClick={()=>openNotification(notification)}>
                         <div className={styles.notificationContent}>
                             <div>
                           <h5>
@@ -70,24 +87,20 @@ export const Notifications = ()=>{
         </div>
         <div className={`${styles.notificationSection} ${styles.dismissed}`}>
         {
-                notificationsPrev.length > 0  && <h4>Previous ({notificationsPrev.length})</h4>
+                notificationsPrev&& notificationsPrev.length > 0  &&<div className={styles.notificationCreateHeader}> <h4>Previous ({notificationsPrev.length})</h4></div>
             }
             {
-                notificationsPrev.map(notification=>{
+                notificationsPrev?.map(notification=>{
                     return (
-                        <div className={styles.notificationItem}>
+                        <div className={styles.notificationItem} onClick={()=>openNotification(notification)}>
                         <div className={styles.notificationContent}>
                             <div>
                           <h5>
                             {notification.title}
                           </h5>
-                          {
-                            notification.data ? <a href={notification.data}>
-                            {notification.description}
-                          </a>:<p className={styles.notificationDescription}>
-                            {notification.description}
+                          <p className={styles.notificationDescription}>
+                            {truncateText(notification.description,30)}
                           </p>
-                          }
                           
                           </div>
                           <time className={styles.notificationTime}>2 mins</time>
